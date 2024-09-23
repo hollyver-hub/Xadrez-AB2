7b27ccff6aa4638273378a419e656d453ea4c8c6
Bruno Hollyver Alves dos Mártires <hollyver@hollyver-deb.hollyver>
2024-09-23 13:41:31 -0300

    Renomeando os arquivos

--- assets.py
+++ assets.py
@@ -17,22 +17,22 @@ height_pecas = 55
 
 # Peças brancas:
 
-torre_branca = pygame.image.load('images_refeito/rook-w.svg')
+torre_branca = pygame.image.load('images/rook-w.svg')
 torre_branca = pygame.transform.scale(torre_branca, (width_pecas, height_pecas))
 
-cavalo_branco = pygame.image.load('images_refeito/knight-w.svg')
+cavalo_branco = pygame.image.load('images/knight-w.svg')
 cavalo_branco = pygame.transform.scale(cavalo_branco, (width_pecas, height_pecas))
 
-bispo_branco = pygame.image.load('images_refeito/bishop-w.svg')
+bispo_branco = pygame.image.load('images/bishop-w.svg')
 bispo_branco = pygame.transform.scale(bispo_branco, (width_pecas, height_pecas))
 
-rainha_branca = pygame.image.load('images_refeito/queen-w.svg')
+rainha_branca = pygame.image.load('images/queen-w.svg')
 rainha_branca = pygame.transform.scale(rainha_branca, (width_pecas, height_pecas))
 
-rei_branco = pygame.image.load('images_refeito/king-w.svg')
+rei_branco = pygame.image.load('images/king-w.svg')
 rei_branco = pygame.transform.scale(rei_branco, (width_pecas, height_pecas))
 
-peao_branco = pygame.image.load('images_refeito/pawn-w.svg')
+peao_branco = pygame.image.load('images/pawn-w.svg')
 peao_branco = pygame.transform.scale(peao_branco, (width_pecas, height_pecas))
 
 imagens_pecas_brancas = [torre_branca, cavalo_branco, bispo_branco, rainha_branca, rei_branco, peao_branco]
@@ -41,22 +41,22 @@ pecas_capturadas_branco = []
 
 # Peças pretas:
 
-torre_preta = pygame.image.load('images_refeito/rook-b.svg')
+torre_preta = pygame.image.load('images/rook-b.svg')
 torre_preta = pygame.transform.scale(torre_preta, (width_pecas, height_pecas))
 
-cavalo_preto = pygame.image.load('images_refeito/knight-b.svg')
+cavalo_preto = pygame.image.load('images/knight-b.svg')
 cavalo_preto = pygame.transform.scale(cavalo_preto, (width_pecas, height_pecas))
 
-bispo_preto = pygame.image.load('images_refeito/bishop-b.svg')
+bispo_preto = pygame.image.load('images/bishop-b.svg')
 bispo_preto = pygame.transform.scale(bispo_preto, (width_pecas, height_pecas))
 
-rainha_preto = pygame.image.load('images_refeito/queen-b.svg')
+rainha_preto = pygame.image.load('images/queen-b.svg')
 rainha_preto = pygame.transform.scale(rainha_preto, (width_pecas, height_pecas))
 
-rei_preto = pygame.image.load('images_refeito/king-b.svg')
+rei_preto = pygame.image.load('images/king-b.svg')
 rei_preto = pygame.transform.scale(rei_preto, (width_pecas, height_pecas))
 
-peao_preto = pygame.image.load('images_refeito/pawn-b.svg')
+peao_preto = pygame.image.load('images/pawn-b.svg')
 peao_preto = pygame.transform.scale(peao_preto, (width_pecas, height_pecas))
 
 imagens_pecas_pretas = [torre_preta, cavalo_preto, bispo_preto, rainha_preto, rei_preto, peao_preto]
